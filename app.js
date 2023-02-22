a7b098ea6d5dc0ae70aa0aacf7ab1374c23cb599
davilux <mx.davilux@gmail.com>
2023-02-22 12:50:25 -0500

    JWT authentication check is done through custom middleware.

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch(error) {
+    next(error);
+  }
+};
+
 // routes
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
@@ -23,22 +34,21 @@ app.post('/api/auth', async(req, res, next)=> {
   }
 });
 
-app.get('/api/auth', async(req, res, next)=> {
+app.get('/api/auth', requireToken, async(req, res, next)=> {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   }
   catch(ex){
     next(ex);
   }
 });
 
-app.get('/api/users/:id/notes', async(req, res, next)=> {
+app.get('/api/users/:id/notes', requireToken, async(req, res, next)=> {
   try {
-    const user = await User.byToken(req.headers.authorization)
-    if(user){
+    if(req.user){
       res.send( await Note.findAll({
         where : {
-          userId : user.id
+          userId : req.user.id
         }
       }));
     }
